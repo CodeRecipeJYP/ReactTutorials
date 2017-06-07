fce29ee03f8c7951e4a33e3d6d40e2b8f2cc94e7
pjo901018 <pjo901018@korea.ac.kr>
2017-06-08 02:20:08 +0900

    Timer구현, onStart, onStop, onReset구현

--- app.jsx
+++ app.jsx
@@ -21,11 +21,35 @@ var Stopwatch = React.createClass({
   getInitialState: function() {
     return {
       running: false,
+      elapsedTime: 0,
+      previousTime: 0,
     }
   },
 
+  componentDidMount: function() {
+    this.interval = setInterval(this.onTick, 100);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  onTick: function() {
+    if (this.state.running) {
+      var now = Date.now();
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+      })
+    }
+    // console.log('onTick');
+  },
+
   onStart: function() {
-    this.setState({ running: true });
+    this.setState({
+      running: true,
+      previousTime: Date.now(),
+    });
   },
 
   onStop: function() {
@@ -33,14 +57,20 @@ var Stopwatch = React.createClass({
   },
 
   onReset: function() {
-
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    })
   },
 
   render: function() {
+    var seconds = Math.floor(this.state.elapsedTime / 1000);
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
+        <div className="stopwatch-time">
+          { seconds }
+        </div>
         { this.state.running ?
           <button onClick={this.onStop}>Stop</button> :
           <button onClick={this.onStart}>Start</button>
