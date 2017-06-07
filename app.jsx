176bf9c53ada9da45229b2678b851781be9b4c47
pjo901018 <pjo901018@korea.ac.kr>
2017-06-07 20:09:48 +0900

    Application을 class로 변경

--- app.jsx
+++ app.jsx
@@ -98,32 +98,36 @@ Player.propTypes = {
   score: React.PropTypes.number.isRequired,
 };
 
-function Application(props) {
-  return (
-    <div className="scoreboard">
-      <Header title={props.title}/>
+var Application = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    // players: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+    players: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired,
+    })).isRequired,
+  },
 
-      <div className="players">
-        {props.players.map(function(player) {
-          return <Player name={player.name} score={player.score} key={player.id}/>
-        })}
+  getDefaultProps: function() {
+    return {
+      title: "Scoreboard",
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="scoreboard">
+        <Header title={this.props.title}/>
+
+        <div className="players">
+          {this.props.players.map(function(player) {
+            return <Player name={player.name} score={player.score} key={player.id}/>
+          })}
+        </div>
       </div>
-    </div>
-  );
-}
-
-Application.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  // players: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-  players: React.PropTypes.arrayOf(React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired,
-    id: React.PropTypes.number.isRequired,
-  })).isRequired,
-};
-
-Application.defaultProps = {
-  title: "Scoreboard",
-};
+    );
+  }
+});
 
 ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
