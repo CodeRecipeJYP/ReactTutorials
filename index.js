8934e66c51dfba75db564fc9b8e4b9c0c86f400e
pjo901018 <pjo901018@korea.ac.kr>
2017-06-09 23:38:53 +0900

    Components분리

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import React from "react";
 import { render } from 'react-dom';
 
-import Scoreboard from './src/components/Scoreboard';
+import Scoreboard from './src/containers/Scoreboard';
 import './css/style.css';
 
 
