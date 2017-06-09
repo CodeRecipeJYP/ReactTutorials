358c447359f3a37df0fde4b1b74166417ffe0b85
pjo901018 <pjo901018@korea.ac.kr>
2017-06-10 00:28:52 +0900

    actions, reducers 생성

---
+++ src/actiontypes/player.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jaeyoung on 2017. 6. 9..
+ */
+export const ADD_PLAYER = 'player/ADD_PLAYER';
+export const REMOVE_PLAYER = 'player/REMOVE_PLAYER';
+export const UPDATE_PLAYER_SCORE = 'player/UPDATE_PLAYER_SCORE';
